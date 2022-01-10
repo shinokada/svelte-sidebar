46f448108720ef91b9b91f7dd1a58352eda071b5
Shinichi Okada <okada.shin@gmail.com>
2022-01-10 14:53:50 +0900

    package.json updates.

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
+  },
+
+  preprocess: [preprocess({})],
 };
 
 export default config;
