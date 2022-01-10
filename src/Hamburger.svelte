d475bb7f0f9dbba67b7cb23847430b1e6e5497c1
Shinichi Okada <okada.shin@gmail.com>
2022-01-10 17:05:24 +0900

    Hamburger.svelte update.

--- src/Hamburger.svelte
+++ src/Hamburger.svelte
@@ -1,48 +1,48 @@
 <script>
-	import { open } from './store';
-	function toggleSide() {
-		open.update((n) => (n = !n));
-	}
-	export let barClass;
+  import { open } from "./src/store";
+  function toggleSide() {
+    open.update((n) => (n = !n));
+  }
+  export let barClass;
 </script>
 
 <button class={barClass} class:open={$open} on:click={toggleSide}>
-	<svg width="32" height="24">
-		<line id="top" x1="0" y1="2" x2="32" y2="2" />
-		<line id="middle" x1="0" y1="12" x2="24" y2="12" />
-		<line id="bottom" x1="0" y1="22" x2="32" y2="22" />
-	</svg>
+  <svg width="32" height="24">
+    <line id="top" x1="0" y1="2" x2="32" y2="2" />
+    <line id="middle" x1="0" y1="12" x2="24" y2="12" />
+    <line id="bottom" x1="0" y1="22" x2="32" y2="22" />
+  </svg>
 </button>
 
 <style>
-	svg {
-		min-height: 24px;
-		transition: transform 0.3s ease-in-out;
-	}
+  svg {
+    min-height: 24px;
+    transition: transform 0.3s ease-in-out;
+  }
 
-	svg line {
-		stroke: currentColor;
-		stroke-width: 3;
-		transition: transform 0.3s ease-in-out;
-	}
+  svg line {
+    stroke: currentColor;
+    stroke-width: 3;
+    transition: transform 0.3s ease-in-out;
+  }
 
-	button {
-		z-index: 20;
-	}
+  button {
+    z-index: 20;
+  }
 
-	.open svg {
-		transform: scale(0.7);
-	}
+  .open svg {
+    transform: scale(0.7);
+  }
 
-	.open #top {
-		transform: translate(6px, 0px) rotate(45deg);
-	}
+  .open #top {
+    transform: translate(6px, 0px) rotate(45deg);
+  }
 
-	.open #middle {
-		opacity: 0;
-	}
+  .open #middle {
+    opacity: 0;
+  }
 
-	.open #bottom {
-		transform: translate(-12px, 9px) rotate(-45deg);
-	}
+  .open #bottom {
+    transform: translate(-12px, 9px) rotate(-45deg);
+  }
 </style>
