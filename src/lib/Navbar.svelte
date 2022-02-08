8e1fd28e5741d47515e4e76a24153f9325e0c424
Shinichi Okada <okada.shin@gmail.com>
2022-02-08 17:55:20 +0900

    Mobile style update.

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -7,18 +7,19 @@
   export let hamburgerClass =
     "text-gray-500 hover:text-gray-700 cursor-pointer mr-4 border-none focus:outline-none";
   export let topMenus;
-  export let siteClass = "w-6/12 h-9 text-lg pt-2 p-8";
+  export let siteClass = "w-full h-9 text-lg pt-1 pl-12";
   export let siteText;
   export let topul =
     "flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-md md:font-medium pt-1";
   export let topli =
     "block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
   export let topMenuDiv =
-    "container flex flex-wrap justify-end items-center mx-auto dark:bg-gray-800";
+    "container flex flex-wrap justify-end mx-auto dark:bg-gray-800";
+  export let navClass = "flex relative";
 </script>
 
 <header class={headerClass}>
-  <nav class="flex">
+  <nav class={navClass}>
     <Hamburger class={hamburgerClass} />
     <div class={siteClass}>
       <div class={siteText}><a href="/">{siteName}</a></div>
