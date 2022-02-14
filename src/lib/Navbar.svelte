24074ae4d65e39dfa37f6751956624f9530e8bdd
Shinichi Okada <okada.shin@gmail.com>
2022-02-14 18:58:10 +0900

    Removed dark color for the top menu.

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -13,8 +13,7 @@
     "flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-md md:font-medium pt-1";
   export let topli =
     "block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
-  export let topMenuDiv =
-    "container flex flex-wrap justify-end mx-auto dark:bg-gray-800";
+  export let topMenuDiv = "container flex flex-wrap justify-end mx-auto";
   export let navClass = "flex relative";
 </script>
 
