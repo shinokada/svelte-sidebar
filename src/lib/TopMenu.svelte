5ec3070d8bdb1e3119b2ede0e8d589451e1401ea
Shinichi Okada <okada.shin@gmail.com>
2022-03-12 13:15:08 +0900

    fix: h-10 w-10 to h-8 w-8.

--- src/lib/TopMenu.svelte
+++ src/lib/TopMenu.svelte
@@ -1,120 +1,107 @@
 <script>
-  import { page } from "$app/stores";
+	import { page } from '$app/stores';
 
-  export let activeDropdownDiv;
-  export let activeChildLi;
-  export let buttonClass;
-  export let childLi;
-  export let dropdownLi;
-  export let topMenus;
-  export let topMenuDiv;
-  export let topul;
-  export let topli;
+	export let activeDropdownDiv;
+	export let activeChildLi;
+	export let buttonClass;
+	export let childLi;
+	export let dropdownLi;
+	export let topMenus;
+	export let topMenuDiv;
+	export let topul;
+	export let topli;
 
-  let hidden = true;
-  let block = false;
-  let activeDropdown = undefined;
-  const handleDropdown = (dropdown) => {
-    console.log("dropdown.id", dropdown.id);
-    hidden = !hidden;
-    block = !block;
-    activeDropdown = dropdown.id;
-    console.log("activeDropdown: ", activeDropdown);
-  };
+	let hidden = true;
+	let block = false;
+	let activeDropdown = undefined;
+	const handleDropdown = (dropdown) => {
+		console.log('dropdown.id', dropdown.id);
+		hidden = !hidden;
+		block = !block;
+		activeDropdown = dropdown.id;
+		console.log('activeDropdown: ', activeDropdown);
+	};
 
-  let barHidden = true;
-  const handleClickbtn = () => {
-    barHidden = !barHidden;
-  };
+	let barHidden = true;
+	const handleClickbtn = () => {
+		barHidden = !barHidden;
+	};
 </script>
 
 <div class={topMenuDiv}>
-  <button
-    on:click={handleClickbtn}
-    data-collapse-toggle="mobile-menu"
-    type="button"
-    class={buttonClass}
-    aria-controls="mobile-menu-2"
-    aria-expanded="false"
-  >
-    <span class="sr-only">Open main menu</span>
-    <svg
-      class="w-8 h-8"
-      fill="currentColor"
-      viewBox="0 0 20 20"
-      xmlns="http://www.w3.org/2000/svg"
-      ><path
-        fill-rule="evenodd"
-        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-        clip-rule="evenodd"
-      /></svg
-    >
-    <svg
-      class="hidden w-10 h-10"
-      fill="currentColor"
-      viewBox="0 0 20 20"
-      xmlns="http://www.w3.org/2000/svg"
-      ><path
-        fill-rule="evenodd"
-        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-        clip-rule="evenodd"
-      /></svg
-    >
-  </button>
-  <div
-    class:hidden={barHidden}
-    class="w-full md:block md:w-auto"
-    id="mobile-menu"
-  >
-    <ul class={topul}>
-      {#each topMenus as { id, name, link, rel, child }}
-        {#if child}
-          <li>
-            <button on:click={handleDropdown({ id })} class={dropdownLi}
-              >{name}
-              <svg
-                class="ml-1 w-4 h-4"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-                ><path
-                  fill-rule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                /></svg
-              ></button
-            >
-            {#if activeDropdown === id}
-              <!-- Dropdown menu -->
-              <div
-                class:hidden
-                class:block
-                class={activeDropdownDiv}
-                style="position: absolute; margin: 0px;"
-              >
-                <ul class="py-1" aria-labelledby="dropdownLargeButton">
-                  {#each child as item}
-                    <li>
-                      <a href={item.link} {rel} class={activeChildLi}
-                        >{item.name}</a
-                      >
-                    </li>
-                  {/each}
-                </ul>
-              </div>
-            {/if}
-          </li>
-        {:else}
-          <li class={topli}>
-            <a
-              class:active={$page.url.pathname === link}
-              href={link}
-              {rel}
-              class={childLi}>{name}</a
-            >
-          </li>
-        {/if}
-      {/each}
-    </ul>
-  </div>
+	<button
+		on:click={handleClickbtn}
+		data-collapse-toggle="mobile-menu"
+		type="button"
+		class={buttonClass}
+		aria-controls="mobile-menu-2"
+		aria-expanded="false"
+	>
+		<span class="sr-only">Open main menu</span>
+		<svg class="w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
+			><path
+				fill-rule="evenodd"
+				d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+				clip-rule="evenodd"
+			/></svg
+		>
+		<svg
+			class="hidden w-8 h-8"
+			fill="currentColor"
+			viewBox="0 0 20 20"
+			xmlns="http://www.w3.org/2000/svg"
+			><path
+				fill-rule="evenodd"
+				d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+				clip-rule="evenodd"
+			/></svg
+		>
+	</button>
+	<div class:hidden={barHidden} class="w-full md:block md:w-auto" id="mobile-menu">
+		<ul class={topul}>
+			{#each topMenus as { id, name, link, rel, child }}
+				{#if child}
+					<li>
+						<button on:click={handleDropdown({ id })} class={dropdownLi}
+							>{name}
+							<svg
+								class="ml-1 w-4 h-4"
+								fill="currentColor"
+								viewBox="0 0 20 20"
+								xmlns="http://www.w3.org/2000/svg"
+								><path
+									fill-rule="evenodd"
+									d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+									clip-rule="evenodd"
+								/></svg
+							></button
+						>
+						{#if activeDropdown === id}
+							<!-- Dropdown menu -->
+							<div
+								class:hidden
+								class:block
+								class={activeDropdownDiv}
+								style="position: absolute; margin: 0px;"
+							>
+								<ul class="py-1" aria-labelledby="dropdownLargeButton">
+									{#each child as item}
+										<li>
+											<a href={item.link} {rel} class={activeChildLi}>{item.name}</a>
+										</li>
+									{/each}
+								</ul>
+							</div>
+						{/if}
+					</li>
+				{:else}
+					<li class={topli}>
+						<a class:active={$page.url.pathname === link} href={link} {rel} class={childLi}
+							>{name}</a
+						>
+					</li>
+				{/if}
+			{/each}
+		</ul>
+	</div>
 </div>
