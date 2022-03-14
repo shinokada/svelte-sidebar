0ed6359c7fabd210ac7f729d9482d855f4a7ec07
Shinichi Okada <okada.shin@gmail.com>
2022-03-14 14:23:24 +0900

    fix: readme file

--- src/routes/scroll-sidebar.svelte
+++ src/routes/scroll-sidebar.svelte
@@ -1,132 +1,141 @@
 <div class="flex flex-row">
-  <div class="basis-1/4 overflow-scroll h-screen p-8">
-    <ul>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-    </ul>
-    <ul>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-    </ul>
-    <ul>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-    </ul>
-    <ul>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-    </ul>
-    <ul>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-    </ul>
-    <ul>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-      <li>Menu</li>
-    </ul>
-  </div>
-  <div class="basis-3/4 p-8">
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </p>
-  </div>
+	<div class="basis-1/4 h-screen overflow-scroll p-8">
+		<ul>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+		</ul>
+		<ul>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+		</ul>
+		<ul>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+		</ul>
+		<ul>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+		</ul>
+		<ul>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+		</ul>
+		<ul>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+			<li>Menu</li>
+		</ul>
+	</div>
+	<div class="basis-3/4 p-8">
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+	</div>
 </div>
