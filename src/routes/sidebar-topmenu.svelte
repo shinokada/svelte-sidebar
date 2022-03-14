03a53f43826aadd8594b9e9ceb470bad54166794
Shinichi Okada <okada.shin@gmail.com>
2022-03-15 07:14:17 +0900

    feat: add topmenu to sidebar default page.

---
+++ src/routes/sidebar-topmenu.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { Sidebar, TopMenu } from '$lib/index';
+	import { lorem, menuList, topMenus } from './menus';
+	let siteName = 'Svelte-sidebar';
+</script>
+
+<Sidebar lists={menuList} {siteName}>
+	<TopMenu {topMenus} />
+</Sidebar>
+<main class="container mx-auto p-24">
+	<h1 class="text-3xl">
+		{siteName}
+	</h1>
+	<p class="pt-4">
+		{lorem}
+	</p>
+	<p class="pt-4">
+		{lorem}
+	</p>
+	<p class="pt-4">
+		{lorem}
+	</p>
+	<p class="pt-4">
+		{lorem}
+	</p>
+</main>
