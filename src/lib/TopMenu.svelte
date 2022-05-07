eaaf5b9b31c89b5906daa35b13fb5a26be23566f
Shinichi Okada <okada.shin@gmail.com>
2022-05-08 05:37:15 +0900

    fix: add activeChildLi to TopMenu

--- src/lib/TopMenu.svelte
+++ src/lib/TopMenu.svelte
@@ -10,11 +10,16 @@
 		'block py-2 pr-4 pl-3  text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-sm';
 
 	export let topMenuDiv: string = 'container flex flex-wrap justify-end mx-auto';
+
 	export let topMenus: TopMenuType[];
+
 	export let topul: string =
 		'flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-md md:font-medium pt-1';
+
 	export let topli: string =
 		'block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0';
+
+	export let activeChildLi = 'block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100';
 
 	let barHidden = true;
 	const handleClickbtn = () => {
