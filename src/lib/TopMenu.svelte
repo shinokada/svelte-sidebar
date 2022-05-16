c2fba339a332a9f1bc8c5bb4e5c6d74ebe596450
Shinichi Okada <okada.shin@gmail.com>
2022-05-17 08:27:05 +0900

    fix: add topDiv to TopMenu

--- src/lib/TopMenu.svelte
+++ src/lib/TopMenu.svelte
@@ -3,6 +3,8 @@
 	import Dropdown from './Dropdown.svelte';
 	import type { TopMenuType } from './types';
 
+	export let topDiv: string = 'w-full md:block md:w-auto';
+
 	export let buttonClass: string = 'inline-flex items-center text-base text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200';
 
 	export let childLi: string = 'block py-2 pr-4 pl-3  text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-base';
@@ -33,7 +35,7 @@
 		<svg class="w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" /></svg>
 		<svg class="hidden w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
 	</button>
-	<div class:hidden={barHidden} class="w-full md:block md:w-auto" id="mobile-menu">
+	<div class:hidden={barHidden} class={topDiv} id="mobile-menu">
 		<ul class={topul}>
 			{#each topMenus as { id, name, href, rel, child }}
 				{#if child}
